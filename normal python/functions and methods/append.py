725545399478aeb9e5b8580de32e8c241371c9d7
murraycoding <67034749+murraycoding@users.noreply.github.com>
2020-09-09 13:31:42 -0400

    Add files via upload

---
+++ normal python/functions and methods/append.py
@@ -0,0 +1,18 @@
+# example of append method
+
+# appending to an existing list
+picnic = []
+cheese_and_crackers = ["Cheese","Crackers"]
+
+for item in cheese_and_crackers:
+    picnic.append(item)
+
+print(picnic)
+
+# appending things to an already existing list
+extra_things = ["Wine","Grapes","Strawberries"]
+
+for thing in extra_things:
+    picnic.append(thing)
+
+print(picnic)
