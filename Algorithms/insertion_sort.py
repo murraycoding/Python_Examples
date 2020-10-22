241ec1d8691741c47cbaa7a8c2a0ea06d219e1e5
murraycoding <67034749+murraycoding@users.noreply.github.com>
2020-10-22 11:57:34 -0400

    #7 fixed bug in algorithm

--- Algorithms/insertion_sort.py
+++ Algorithms/insertion_sort.py
@@ -1,11 +1,11 @@
 # insertion sort notes
 
-example_list_1: list = [1,4,6,5,8,10,12,24,11,12,12,15]
+example_list_1: list = [4,6,5,8,10,1,12,24,11,5,5,5,5,12,12,15]
 
 for i in range(len(example_list_1)):
     # only considers the range from 0 to i as I gets bigger 
     for j in range(0,i):
         # if the next in line is smaller than the one before, switch them
-        if example_list_1[j+1] < example_list_1[j]:
-            example_list_1[j], example_list_1[j+1] = example_list_1[j+1], example_list_1[j]
+        if example_list_1[i-j] <= example_list_1[i-j-1]:
+            example_list_1[i-j-1], example_list_1[i-j] = example_list_1[i-j], example_list_1[i-j-1]
 
